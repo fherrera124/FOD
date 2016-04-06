59b8dbc3e909bbe8e2dffd1eae1ba251d66b3865
elganzua124 <fran.herrera124@gmail.com>
2016-04-06 01:30:05 -0300

    act

---
+++ practicas/p2/p2e4.pas
@@ -0,0 +1,27 @@
+{
+* El encargado de ventas de un negocio de productos de limpieza desea
+* administrar el stock de los productos que vende. Para ello, genera un
+* archivo maestro donde figuran todos los productos que comercializa.
+* Se cada producto se maneja la siguiente información: código de
+* producto, nombre comercial, precio de venta, stock actual y stock
+* mínimo. Diariamente se genera un archivo detalle donde se registran
+* todas las ventas de productos realizadas. De cada venta se registra:
+* código de producto y cantidad de unidades vendidas. Se pide realizar
+* un programa con opciones para:
+* a. Crear el archivo maestro a partir de un archivo de texto llamado
+* “productos.txt”.
+* b. Listar el contenido del archivo maestro en un archivo de texto
+* llamado “reporte.txt”, listando de a un producto por línea.
+* c. Crear un archivo detalle de ventas a partir de en un archivo de
+* texto llamado “ventas.txt”.
+* d. Listar en pantalla el contenido del archivo detalle de ventas.
+* e. Actualizar el archivo maestro con el archivo detalle, sabiendo que:
+*   -Ambos archivos están ordenados por código de producto.
+*   -Cada registro del maestro puede ser actualizado por 0, 1 ó más
+*    registros del archivo detalle.
+*   -El archivo detalle sólo contiene registros que están en el archivo
+*    maestro.
+* f. Listar en un archivo de texto llamado “stock_minimo.txt” aquellos
+* productos cuyo stock actual esté por debajo del stock mínimo permitido
+* .
+}
